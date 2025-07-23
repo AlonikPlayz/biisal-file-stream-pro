3f98e8c1940df3ded1880dc444450976dfa007ba
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2025-07-23 11:54:06 +0530

    Update __main__.py

--- biisal/__main__.py
+++ biisal/__main__.py
@@ -34,7 +34,6 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 ppath = "biisal/bot/plugins/*.py"
 files = glob.glob(ppath)
 StreamBot.start()
-
 loop = asyncio.get_event_loop()
 
 
@@ -90,8 +89,7 @@ async def start_services():
     try: 
         await StreamBot.send_message(chat_id=Var.OWNER_ID[0] ,text='<b>ᴊᴀɪ sʜʀᴇᴇ ᴋʀɪsʜɴᴀ 😎\nʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
     except Exception as e:
-        print(f'got this err to send restart msg to owner : {e}')
-    await StreamBot.listen()
+        print(f'got this err to send restart msg to owner : {e}'
     await idle()
 
 if __name__ == '__main__':
