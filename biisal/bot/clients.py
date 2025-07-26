ccbcd9c660eba055213d8a3056ae4c2bd5120a03
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2025-07-26 20:02:45 +0530

    Update clients.py

--- biisal/bot/clients.py
+++ biisal/bot/clients.py
@@ -1,45 +1,74 @@
-# (c) biisal (c) adarsh-goel
+# Thunder/bot/clients.py
 
 import asyncio
-import logging
-from ..vars import Var
-from pyrogram import Client
-from biisal.utils.config_parser import TokenParser
-from . import multi_clients, work_loads, StreamBot
 
+from pyrogram import Client
+
+from biisal.bot import StreamBot, multi_clients, work_loads
+from biisal.utils.config_parser import TokenParser
+from biisal.utils.handler import handle_flood_wait
+from biisal.utils.logger import logger
+from biisal.vars import Var
+
+async def cleanup_clients():
+    for client in multi_clients.values():
+        try:
+            await handle_flood_wait(client.stop)
+        except Exception as e:
+            logger.error(f"Error stopping client: {e}", exc_info=True)
 
 async def initialize_clients():
+    print("╠══════════════════ INITIALIZING CLIENTS ═══════════════════╣")
     multi_clients[0] = StreamBot
     work_loads[0] = 0
-    all_tokens = TokenParser().parse_from_env()
-    if not all_tokens:
-        print("No additional clients found, using default client")
+    print("   ✓ Primary client initialized")
+    try:
+        all_tokens = TokenParser().parse_from_env()
+        if not all_tokens:
+            print("   ◎ No additional clients found.")
+            return
+    except Exception as e:
+        logger.error(f"   ✖ Error parsing additional tokens: {e}", exc_info=True)
+        print("   ▶ Primary client will be used.")
         return
-    
+
     async def start_client(client_id, token):
         try:
-            print(f"Starting - Client {client_id}")
             if client_id == len(all_tokens):
                 await asyncio.sleep(2)
-                print("This will take some time, please wait...")
-            client = await Client(
-                name=str(client_id),
-                api_id=Var.API_ID,
+            client = Client(
                 api_hash=Var.API_HASH,
+                api_id=Var.API_ID,
                 bot_token=token,
-                sleep_threshold=Var.SLEEP_THRESHOLD,
+                in_memory=True,
+                name=str(client_id),
                 no_updates=True,
-                in_memory=True
-            ).start()
+                max_concurrent_transmissions=1000,
+                sleep_threshold=Var.SLEEP_THRESHOLD
+            )
+            await handle_flood_wait(client.start)
             work_loads[client_id] = 0
+            print(f"   ◎ Client ID {client_id} started")
             return client_id, client
-        except Exception:
-            logging.error(f"Failed starting Client - {client_id} Error:", exc_info=True)
-    
-    clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items()])
+        except Exception as e:
+            logger.error(f"   ✖ Failed to start Client ID {client_id}. Error: {e}", exc_info=True)
+            return None
+
+    clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items() if token])
+    clients = [client for client in clients if client]
+
     multi_clients.update(dict(clients))
-    if len(multi_clients) != 1:
+    
+    if len(multi_clients) > 1:
         Var.MULTI_CLIENT = True
-        print("Multi-Client Mode Enabled")
+        print("╠══════════════════════ MULTI-CLIENT ═══════════════════════╣")
+        print(f"   ◎ Total Clients: {len(multi_clients)} (Including primary client)")
+        
+        print("   ▶ Initial workload distribution:")
+        for client_id, load in work_loads.items():
+            print(f"   • Client {client_id}: {load} tasks")
+            
     else:
-        print("No additional clients were initialized, using default client")
+        print("╠═══════════════════════════════════════════════════════════╣")
+        print("   ▶ No additional clients were initialized")
+        print("   ▶ Primary client will handle all requests")
